6d176b6a0adfcaaf71abe0ef19577d6c5f15ec4b
Doug-Sales <douglasmsales28@outlook.com>
2022-09-30 20:20:41 -0300

    Abstração

--- node.js/FUNDAMENTOS/10_abstracao_input/index.js
+++ node.js/FUNDAMENTOS/10_abstracao_input/index.js
@@ -1,16 +1,19 @@
-const inquirer = require('inquirer')
-
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 inquirer
     .prompt([{
-        name: 'Nota_1',
+        name: 'nota_1',
         message: 'Digite a primeira nota: ',
     },
     {
-        name: 'Nota_2',
+        name: 'nota_2',
         message: 'Digite a nota 2: ',
     }]).then((resposta) => {
-        console.log(resposta)
+        const nota_1 = parseInt(resposta.nota_1);
+        const nota_2 = parseInt(resposta.nota_2);
+        const media = ((nota_1 + nota_2) / 2);
+        console.log(chalk.green.bgCyan(`${media}`))
     }).catch((err) => {
         console.log(err)
     });
