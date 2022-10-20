0031680c1c266e33fcc4f80174cceb11f31b1969
Doug-Sales <douglasmsales28@outlook.com>
2022-10-20 14:01:30 -0300

    Atualizando

--- node.js/CORE_MODULES/2_retornando_html/index.js
+++ node.js/CORE_MODULES/2_retornando_html/index.js
@@ -5,7 +5,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('contenty-Type', 'text/html')
-    res.end('<h1>Texto inserido no servidor</h1>')
+    res.end('<h1>Texto inserido no servidor</h1><p>Texto atualizado</p>')
 })
 
 server.listen(port, () => {
